4ead374db1d2adb5f8f26a8320be134d4358237e
Sameer yadav <sameer.manoj2005@gmail.com>
2025-07-10 11:31:00 +0530

    Entry animation

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,13 +1,14 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import toast from "react-hot-toast";
 import AuthStore from "../Stores/AuthStore";
+import { motion } from "motion/react";
 
 const Register = () => {
-  const {handleRegister,UserData} = AuthStore();
+  const { handleRegister, UserData } = AuthStore();
 
   // states
   const [showPassword, setshowPassword] = useState(false);
@@ -20,7 +21,7 @@ const Register = () => {
 
   const submitRef = useRef(null);
 
-// ..states
+  // ..states
   const handleSignUp = (e) => {
     e.preventDefault();
 
@@ -34,43 +35,61 @@ const Register = () => {
       return;
     }
 
-const promise = new Promise((resolve, reject) => {
-  if (logoWrapperRef.current && submitRef.current) {
-    logoWrapperRef.current.classList.add("rocket-launch");
+    const promise = new Promise((resolve, reject) => {
+      if (logoWrapperRef.current && submitRef.current) {
+        logoWrapperRef.current.classList.add("rocket-launch");
 
-    // First add base class
-    submitRef.current.classList.add('submit-btn');
+        // First add base class
+        submitRef.current.classList.add("submit-btn");
 
-    // Then force reflow to restart animation
-    void submitRef.current.offsetWidth;
+        // Then force reflow to restart animation
+        void submitRef.current.offsetWidth;
 
-    // Add animation class to trigger width transition
-    submitRef.current.classList.add('animate');
-    submitRef.current.setAttribute('disabled', 'true');
+        // Add animation class to trigger width transition
+        submitRef.current.classList.add("animate");
+        submitRef.current.setAttribute("disabled", "true");
 
-    setTimeout(() => {
-      logoWrapperRef.current.classList.remove("rocket-launch"); // Remove animate to allow re-trigger
-      resolve('Registered successfully!');
-    }, 1300);
-  }
-})
-.then(msg => new Promise((resolve) => setTimeout(() => {      
-  submitRef.current.classList.remove('animate');
-  resolve(msg)
-    // submitRef.current.removeAttribute('disabled');
-    submitRef.current.innerHTML = 'wait ...'
-    submitRef.current.style.color = 'skyblue'
-}, 1000)))
-.then(msg => {
-  toast.success(msg);
-});
+        setTimeout(() => {
+          logoWrapperRef.current.classList.remove("rocket-launch"); // Remove animate to allow re-trigger
+          resolve("Registered successfully!");
+        }, 1300);
+      }
+    })
+      .then(
+        (msg) =>
+          new Promise((resolve) =>
+            setTimeout(() => {
+              submitRef.current.classList.remove("animate");
+              resolve(msg);
+              // submitRef.current.removeAttribute('disabled');
+              submitRef.current.innerHTML = "wait ...";
+              submitRef.current.style.color = "skyblue";
+            }, 1000)
+          )
+      )
+      .then((msg) => {
+        toast.success(msg);
+      });
 
-    handleRegister(name,mail,password);
+    handleRegister(name, mail, password);
     // console.log(UserData);
   };
 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, height: 0, y: 100, perspective: 200}}
+      animate={{
+        // rotate: -360,
+        opacity: 1,
+        y: 0,
+        x: 0,
+        height: 380,
+      }}
+      style={{
+        overflow: "hidden",
+      }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+
       className="Register-Container"
       data-scroll
       data-scroll-speed="1.5 "
@@ -79,14 +98,18 @@ const promise = new Promise((resolve, reject) => {
       <div className="FormWrapper">
         <div className="registerHead">
           <div className="icon-wrapper" ref={logoWrapperRef}>
-            <img
-              src="/Rocket.png"
-              className="fastchat-icon"
-            />
+            <img src="/Rocket.png" className="fastchat-icon" />
             <div className="rocket-fire"></div>
             <div className="rocket-fire small"></div>
           </div>
-          <p className="FastChat-text">FastChat</p>
+          <motion.p
+              initial={{ opacity: 0, y: 300, rotate: 360 }}
+              animate={{ opacity: 1, y: 0, rotate: 0 }}
+              transition={{ delay: 0.2, duration: 0.5, ease: "easeInOut" }}
+            className="FastChat-text"
+          >
+            FastChat
+          </motion.p>
         </div>
         <div className="register">
           <p>Register</p>
@@ -121,13 +144,18 @@ const promise = new Promise((resolve, reject) => {
             <FontAwesomeIcon icon={!showPassword ? faEyeSlash : faEye} />
           </span>
 
-          <button type="submit" ref={submitRef}>Sign up</button>
+          <button type="submit" ref={submitRef}>
+            Sign up
+          </button>
         </form>
         <div className="text">
-          you have an account? <span><Link to={"/Login"}>Login</Link></span>
+          you have an account?{" "}
+          <span>
+            <Link to={"/Login"}>Login</Link>
+          </span>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
